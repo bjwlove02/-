5fc93571030c4141ea1120492c45242617586081
bjwlove02 <20171176@vision.hoseo.edu>
2018-09-11 13:12:12 +0900

    Hello World - 9월 11일 - PP02.BreakUpHelloSDL

--- GamePramework/PP02.BreakUpHelloSDL/main.cpp
+++ GamePramework/PP02.BreakUpHelloSDL/main.cpp
@@ -3,16 +3,13 @@
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-void init(){}
-void render(){}
-void update(){}
-void handleEvents(){}
-void clean(){}
+bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+void render();
 
 bool g_bRunning = false;
 
 bool init(const char* title, int xpos, int ypos, int width, int height, int flags) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {\
 		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 		if (g_pWindow != 0) {
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
