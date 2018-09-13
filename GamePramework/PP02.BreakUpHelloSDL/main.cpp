c46ee8d66d02e28955de9582253a190320746389
bjwlove02 <20171176@vision.hoseo.edu>
2018-09-13 17:27:37 +0900

    Hello World - 9월 13일 - PP03.GameClass

--- GamePramework/PP02.BreakUpHelloSDL/main.cpp
+++ GamePramework/PP02.BreakUpHelloSDL/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[]) {
 }
 
 bool init(const char* title, int xpos, int ypos, int width, int height, int flags) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {\
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
 		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 		if (g_pWindow != 0) {
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
