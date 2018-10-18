0659ba30193529ffba050ca9e26077d076e1665b
bjwlove02 <20171176@vision.hoseo.edu>
2018-10-18 17:42:28 +0900

    - 10월 18일 - PP09.Inheritance

---
+++ GamePramework/PP09.Inheritance/Game.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "TextureManager.h"
+#include "GameObject.h"
+#include "Player.h"
+
+class Game
+{
+	SDL_Texture* m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+	GameObject m_go;
+	Player m_player;
+	int m_currentFrame;
+
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+};
