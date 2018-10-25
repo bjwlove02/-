4383f537b921774e39567c2e568ad883eb193736
bjwlove02 <20171176@vision.hoseo.edu>
2018-10-25 18:55:09 +0900

    - 10월 25일 - PP12.Vector2D

---
+++ GamePramework/PP12.Vector2D/Game.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "TextureManager.h"
+#include "GameObject.h"
+#include "Player.h"
+#include "Enemy.h"
+#include "Vector2D.h"
+
+class Game
+{
+public:
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+
+	static Game* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
+private:
+	Game() {}
+	static Game* s_pInstance;
+
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+
+	std::vector<GameObject*> m_gameObjects;
+
+	bool m_bRunning;
+	
+};
+typedef Game TheGame;
---
+++ GamePramework/새 폴더/GameObject.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "TextureManager.h"
+#include "LoaderParams.h"
+
+class GameObject
+{
+public:
+	virtual void draw() = 0;
+	virtual void update() = 0;
+	virtual void clean() = 0;
+protected:
+	GameObject(const LoaderParams* pParams) {}
+	virtual ~GameObject() {}
+};
