4a702d0b5b91dc55c1e1cf567fcc29059e54d709
bjwlove02 <20171176@vision.hoseo.edu>
2018-11-01 18:42:07 +0900

    14 Backup

---
+++ GamePramework/PP14.MInputHandler/Game.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "Player.h"
+#include "Enemy.h"
+
+class Game
+{
+public:
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	void quit();
+	bool running() { return m_bRunning; }
+
+	static Game* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
+private:
+	Game() {}
+	static Game* s_pInstance;
+
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+
+	std::vector<GameObject*> m_gameObjects;
+
+	bool m_bRunning;
+	
+};
+typedef Game TheGame;
---
+++ GamePramework/PP14.MInputHandler/InputHandler.cpp
@@ -0,0 +1,163 @@
+#include "InputHandler.h"
+#include "Game.h"
+
+InputHandler* InputHandler::s_pInstance = 0;
+
+std::vector <bool> m_mouseButtonStates;
+bool InputHandler::isKeyDown(SDL_Scancode key) {
+	if (m_keystates != 0) {
+		if (m_keystates[key] == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	return false;
+}
+
+InputHandler::InputHandler()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		m_mouseButtonStates.push_back(false);
+	}
+}
+
+void InputHandler::clean()
+{
+	
+}
+
+Vector2D* InputHandler::getMousePosition()
+{
+	return m_mousePosition;
+}
+
+void InputHandler::update()
+{
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT)
+		{
+			TheGame::Instance()->clean();
+		}
+		if (event.type == SDL_KEYUP) {
+			m_keystates = SDL_GetKeyboardState(0);
+		}
+
+		if (event.type == SDL_KEYDOWN) {
+			m_keystates = SDL_GetKeyboardState(0);
+		}
+
+		if (event.type == SDL_MOUSEMOTION)
+		{
+			m_mousePosition->setX(event.motion.x);
+			m_mousePosition->setY(event.motion.y);
+		}
+		else if (event.type == SDL_MOUSEBUTTONDOWN)
+		{
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				m_mouseButtonStates[LEFT] = true;
+			}
+			if (event.button.button == SDL_BUTTON_MIDDLE)
+			{
+				m_mouseButtonStates[MIDDLE] = true;
+			}
+			if (event.button.button == SDL_BUTTON_RIGHT)
+			{
+				m_mouseButtonStates[RIGHT] = true;
+			}
+		}
+		else if (event.type == SDL_MOUSEBUTTONUP)
+		{
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				m_mouseButtonStates[LEFT] = false;
+			}
+			if (event.button.button == SDL_BUTTON_MIDDLE)
+			{
+				m_mouseButtonStates[MIDDLE] = false;
+			}
+			if (event.button.button == SDL_BUTTON_RIGHT)
+			{
+				m_mouseButtonStates[RIGHT] = false;
+			}
+		}
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			TheGame::Instance()->clean();
+			break;
+		case SDL_MOUSEMOTION:
+			onMouseMove(event);
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			onMouseButtonDown(event);
+			break;
+		case SDL_MOUSEBUTTONUP:
+			onMouseButtonUp(event);
+			break;
+		case SDL_KEYDOWN:
+			onKeyDown();
+			break;
+		case SDL_KEYUP:
+			onKeyUp();
+			break;
+		default:
+			break;
+		}
+
+	}
+}
+
+bool InputHandler::getMouseButtonState(int buttonNumber)
+{
+	return m_mouseButtonStates[buttonNumber];
+}
+
+void InputHandler::onMouseMove(SDL_Event event) {
+	m_mousePosition->setX(event.motion.x);
+	m_mousePosition->setY(event.motion.y);
+}
+
+void InputHandler::onMouseButtonDown(SDL_Event event) {
+	if (event.button.button == SDL_BUTTON_LEFT)
+	{
+		m_mouseButtonStates[LEFT] = true;
+	}
+	if (event.button.button == SDL_BUTTON_MIDDLE)
+	{
+		m_mouseButtonStates[MIDDLE] = true;
+	}
+	if (event.button.button == SDL_BUTTON_RIGHT)
+	{
+		m_mouseButtonStates[RIGHT] = true;
+	}
+}
+
+void InputHandler::onMouseButtonUp(SDL_Event event) {
+	if (event.button.button == SDL_BUTTON_LEFT)
+	{
+		m_mouseButtonStates[LEFT] = false;
+	}
+	if (event.button.button == SDL_BUTTON_MIDDLE)
+	{
+		m_mouseButtonStates[MIDDLE] = false;
+	}
+	if (event.button.button == SDL_BUTTON_RIGHT)
+	{
+		m_mouseButtonStates[RIGHT] = false;
+	}
+}
+
+void InputHandler::onKeyDown() {
+	m_keystates = SDL_GetKeyboardState(0);
+}
+
+void InputHandler::onKeyUp() {
+	m_keystates = SDL_GetKeyboardState(0);
+}
